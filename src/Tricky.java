801f4b0d6d58d6eb5115088388bb30ee17392819
Salfurhud <37315069+Salfurhud@users.noreply.github.com>
2018-03-12 21:08:09 -0500

    working

--- src/Tricky.java
+++ src/Tricky.java
@@ -1,14 +1,28 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Alfurhud Solomon
+  	Date: March 3, 2018
 */
 
 // 1. Your predicted output 
  
 
 public class Tricky {
-
+    public static void main(String[] args) {
+        message1();
+        message2();
+        System.out.println("Done with main.");
+    }
+    
+    public static void message1() {
+        System.out.println("This is message1.");
+    }
+    
+    public static void message2() {
+        System.out.println("This is message2.");
+        message1();
+        System.out.println("Done with message 2.");
+    }
 }
 
 // 3. Were you correct? Explain any differences
