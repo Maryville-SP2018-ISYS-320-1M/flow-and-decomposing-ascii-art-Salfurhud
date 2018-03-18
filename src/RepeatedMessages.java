71e834e30f39f6c4950558b40cc2307b000f87a7
Salfurhud <37315069+Salfurhud@users.noreply.github.com>
2018-03-18 18:07:46 -0500

    Add files via upload

---
+++ src/RepeatedMessages.java
@@ -0,0 +1,18 @@
+/*
+  	ISYS 320
+  	Name(s):AlfurhudSolomon
+  	Date:March18,2018
+*/
+
+// Bonus Credit Question
+
+public class RepeatedMessages {
+
+	
+	public static void main(String[] args) {
+		for(int x = 0; x < 1000; x++ ) {
+			System.out.println("Milhouse did not test cootie positive.");
+		}
+	}
+}
+
